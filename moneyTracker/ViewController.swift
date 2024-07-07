e9b3b8b830969701d72d119772b9da1bd775415c
vovatesla <vovan980706@gmail.com>
2024-07-07 17:19:55 +0300

    Initial Commit

---
+++ moneyTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moneyTracker
+//
+//  Created by Бадретдинов Владимир on 07.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
